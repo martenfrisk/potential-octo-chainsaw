a648613f852b64b018fd1b1aa2def4c0cfd5f7f7
Mårten Frisk <marten.frisk@askas.se>
2021-02-22 15:01:57 +0100

    Ta emot props i cart

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -1,3 +1,19 @@
-<div class="absolute z-20 flex flex-col px-4 py-8 transform bg-white border border-blue-400 rounded-md shadow-lg translate-y-2/3 -translate-x-1/3">
-	Cart
-</div>
+<script>
+	export let items
+</script>
+
+{#if items}
+	<div
+		class="fixed z-30 flex flex-col px-4 py-8 transform translate-y-1/2 bg-white border border-blue-400 rounded-md shadow-lg -translate-x-1/3"
+	>
+		{#if items.cartrows}
+			{#each items.cartrows as item}
+				<p>{item.article.title}</p>
+			{/each}
+		{:else}
+			{#each items as item}
+				<p>{item.productName}</p>
+			{/each}
+		{/if}
+	</div>
+{/if}
