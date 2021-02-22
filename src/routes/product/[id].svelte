efe85163dfae8e88e4d28c500734e0808835e5e2
Mårten Frisk <marten.frisk@askas.se>
2021-02-22 10:34:28 +0100

    Hämta extra produkter i preload

--- src/routes/product/[id].svelte
+++ src/routes/product/[id].svelte
@@ -1,5 +1,5 @@
 <script context="module" lang="ts">
-	import { getProductByArtnr } from '../../utils/api'
+	import { getProductByArtnr, getCategory } from '../../utils/api'
 	import type { APIArticle } from '../../methods/types'
 
 	export async function preload({ params }) {
@@ -8,27 +8,52 @@
 			id: product.article.id,
 			productName: product.article.title,
 			productId: product.article.id,
-			cover: product.article.image.normal,
+			cover: product.article.image.normal.split('/')[3].split('?')[0].split('.')[0],
 			price: product.article.price,
 			shortDescription: product.article.short_description,
 			description: product.article.description,
 		}
-		return { product: tempArticle }
+		const res = await getCategory('9', '10')
+		let products = []
+		res.article_list.articles.forEach((article: any, index: number) => {
+			const tempArticle = {
+				id: index,
+				productName: article.title,
+				productId: article.id,
+				cover: article.image.normal.split('/')[3].split('?')[0].split('.')[0],
+				price: article.price,
+				description: article.short_description,
+			}
+			products.push(tempArticle)
+		})
+		return { product: tempArticle, moreProducts: products }
 	}
 </script>
 
 <script lang="typescript">
-	import type { FilteredArticle } from '../../methods/types'
-	export let product: FilteredArticle
+	import type { FilteredArticle, IndexProduct } from '../../methods/types'
+
 	import PSCover from '../../components/PSCover.svelte'
 
 	import Basket from '../../components/icons/Basket.svelte'
 	import StarEmpty from '../../components/icons/StarEmpty.svelte'
+	import MoreProducts from '../../components/MoreProducts.svelte'
+	import { fly } from 'svelte/transition'
+	import { quintIn } from 'svelte/easing'
+	export let product: FilteredArticle, moreProducts: IndexProduct[]
+	const filteredMore = moreProducts
+		.filter(i => i.productId !== product.productId)
+		.slice(0, 8)
 </script>
+
+<svelte:head>
+	<title>Sapper e-commerce - {product.productName}</title>
+</svelte:head>
 
 <div class="relative z-10">
 	<div
 		class="flex flex-wrap justify-center w-5/6 max-w-5xl px-4 py-6 mx-auto my-4 text-white bg-gray-600 bg-opacity-25 border border-gray-500 sm:my-12 sm:px-20 font-quicksand rounded-3xl hero"
+		in:fly={{ y: -50, opacity: 0.5, easing: quintIn }}
 	>
 		<div class="flex flex-col justify-center w-full sm:w-2/5">
 			<p class="text-4xl font-semibold">
@@ -36,7 +61,8 @@
 					>({product.price}:-)</span
 				>
 			</p>
-			<p class="my-2">{@html product.description}</p>
+			<p class="my-2">{product.cover}
+				{@html product.description}</p>
 			<div class="flex">
 				<div class="text-white hover:text-yellow-200">
 					<StarEmpty />
@@ -50,8 +76,7 @@
 			<PSCover cover={product.cover} alt={product.productName} />
 		</div>
 	</div>
-
-
+	<MoreProducts moreProducts={filteredMore} />
 </div>
 
 <style>
